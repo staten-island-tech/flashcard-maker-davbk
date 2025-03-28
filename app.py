6eebce214cbdb2097510370aab11bc8fff12da45
davbk <davidc828@nycstudents.net>
2025-03-28 09:42:05 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 import json
 
-class student_mode:
-    def __init__(self, name, classes, work):
+class User:
+    def __init__(self, name, email):
         self.name = name
-        self.classes = classes
-        self.work = work
+        self.email = email
     def display_info(self):
-        return f"Student: {self.name}, Classes: {self.classes}, Work: {self.work}"
-David = student_mode("David", "Chemistry", "Test")
+        return f"User: {self.name}, Email: {self.email}"
+class Student(User):
+    def __init__(self, name, email, student_id):
+        super().__init__(name, email)  
+        self.student_id = student_id
