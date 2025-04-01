1453b8798493cf72ff3eff92c1ebab8eb2fa7276
davbk <davidc828@nycstudents.net>
2025-04-01 10:09:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ class User:
     def display_info(self):
         return f"User: {self.name}, Email: {self.email}"
 class Student(User):
-    def __init__(self, name, email, student_id):
-        super().__init__(name, email)  
+    def __init__(self, name, email, student_id): 
         self.student_id = student_id
+Class Teacher(User):
+def __init__(self, name, email, teacher_id):
