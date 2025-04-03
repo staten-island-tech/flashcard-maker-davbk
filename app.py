5b06413947921cecbf62d684a1bffeb95f9cc554
davbk <davidc828@nycstudents.net>
2025-04-03 10:14:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ class User:
     def display_info(self):
         return f"User: {self.name}, Email: {self.email}"
 class Student(User):
-    def __init__(self, name, email, student_id): 
-        self.student_id = student_id
-class Teacher(User):
-    def __init__(self, name, email, teacher_id):
-        self.teacher_id = teacher_id
+    def __init__(self, question, answer, streak): 
+        super().__init__(question, answer)
+        self.streak = streak
