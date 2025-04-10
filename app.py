3ee100ff5dc308b9199e433cbedc42cca6a7b8c4
davbk <davidc828@nycstudents.net>
2025-04-10 10:08:19 -0400

    wewee

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
 import json
 
-class Flashcard:
-    def __init__(self, question, answer):
-        self.question = question
-        self.answer = answer
-    def to_dict(self):
+class Teacher:
+    def __init__(self):
+        self.aa = {}
+
+    def makekey(self):
+        key = input("enter a key: ")
+        value = input("enter a value:")
+        self.aa[key] = value
+        print(self.aa)
+
+    def save(self):
+        with open("Flashcards.json", 'w') as file:
+            json.dump(self.aa, file, indent=4)
+
+w = Teacher()
+w.makekey()
+w.save()
+
+"""     def to_dict(self):
         return {"question": self.question, "answer": self.answer}
     def teacher():
-        flashcards = []
+        flashcards = {}
         while True:
             question = input ("Enter a question: ")
             if question == "quit":
                 break
             answer = input("Enter answer: ")
             flashcard = Flashcard(question, answer)
-            flashcards.append(flashcard)
-
+            flashcards.append(flashcard) """
