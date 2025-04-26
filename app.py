1bcfc2f3a5f7440b4f69cbc87d4b6fe40debf4e7
davbk <davidc828@nycstudents.net>
2025-04-26 13:28:01 -0400

    omds finally done

--- app.py
+++ app.py
@@ -1,50 +1,51 @@
+import json
+
 streak = 0
 points = 0
-import json
 
 class Teacher:
     def __init__(self):
-        self.aa = {}
+        self.flashcards = {}
 
-    def makekey(self):
-        key = input("enter a key: ")
-        value = input("enter a value:")
-        self.aa[key] = value
-        print(self.aa)
+    def make_flashcards(self):
+        key = input("Enter a term: ")
+        value = input("Enter the definition: ")
+        self.flashcards[key] = value
 
-    def save(self):
-        with open("Flashcards.json", 'w') as file:
-            json.dump(self.aa, file, indent=4)
+    def save_flashcards(self):
+        with open("Flashcards.json", "w") as f:
+            json.dump(self.flashcards, f)
 
-w = Teacher()
-w.makekey()
-w.save()
-class Flashcards:
-    def __init__(self, answers, questions):
-        self.answers = answers
-        self.questions = questions
 class Student:
-    def __init__(self, teach_instance):
+    def __init__(self):
+        with open("Flashcards.json", "r") as f:
+            self.flashcards = json.load(f)
+
+    def quiz(self):
         global streak
         global points
-        self.teach_instance = teach_instance
-    def start(self):
-        global streak
-        global points
-        answer = input("enter answer:")
-        if answer == answer:
-            streak += 1
-            points += 1
+        for key in self.flashcards:
+            answer = input(f"What is the definition of '{key}'? ")
+            if answer == self.flashcards[key]:
+                print("correct")
+                streak += 1
+                points += 1
+            else:
+                print(f"wrong")
+                streak = 0
+            print(f"Streak: {streak}")
+            print(f"Points: {points}")
 
+mode = input("teacher or student? ")
 
-"""     def to_dict(self):
-        return {"question": self.question, "answer": self.answer}
-    def teacher():
-        flashcards = {}
-        while True:
-            question = input ("Enter a question: ")
-            if question == "quit":
-                break
-            answer = input("Enter answer: ")
-            flashcard = Flashcard(question, answer)
-            flashcards.append(flashcard) """
+if mode == "teacher":
+    t = Teacher()
+    t.make_flashcards()
+    t.save_flashcards()
+
+elif mode == "student":
+    s = Student()
+    s.quiz()
+
+else:
+    exit
